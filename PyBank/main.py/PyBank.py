f01bb8b4cd41e953964449297b44da75c2b67764
KidusGorfe <kidus.gorfe@gmail.com>
2023-09-27 18:53:25 -0500

    No Pandas

--- PyBank/main.py/PyBank.py
+++ PyBank/main.py/PyBank.py
@@ -1,82 +1,78 @@
-import pandas as pd 
-
-#Load in files 
+# Specify the file path
 file_path = "C:\\Users\\kidus\\OneDrive\\Documents\\GitHub\\python-challenge\\PyBank\\Resources\\budget_data.csv"
 
-df = pd.read_csv(file_path)
+# Read data from the CSV file
+with open(file_path, 'r') as file:
+    lines = file.readlines()
 
-#Header
+# Remove the header line
+header = lines.pop(0)
 
-header = print("""Financial Analysis
-----------------------------""")
+# Initialize variables to store financial analysis data
+total_months = 0
+total_profit_losses = 0
+previous_profit_loss = None
+total_change = 0
+greatest_increase = None
+greatest_decrease = None
+greatest_increase_date = None
+greatest_decrease_date = None
 
-#Find Month Count 
-monthCount = df["Date"].value_counts().sum()
+# Process the data
+for line in lines:
+    date, profit_loss = line.strip().split(',')
+    profit_loss = int(profit_loss)
+   
+    # Count months and calculate total profit/losses
+    total_months += 1
+    total_profit_losses += profit_loss
+   
+    # Calculate the change and update the total change
+    if previous_profit_loss is not None:
+        monthly_change = profit_loss - previous_profit_loss
+        total_change += monthly_change
+   
+    # Update greatest increase and decrease
+    if greatest_increase is None or profit_loss > greatest_increase:
+        greatest_increase = profit_loss
+        greatest_increase_date = date
+    if greatest_decrease is None or profit_loss < greatest_decrease:
+        greatest_decrease = profit_loss
+        greatest_decrease_date = date
+   
+    previous_profit_loss = profit_loss
 
-print(f"Total Months: {monthCount}")
+# Calculate the average change
+average_change = total_change / (total_months - 1)
 
-#Find Total Income
-Total = df["Profit/Losses"].sum()
-
-print( f"Total: ${Total}")
-
-#Find Average Change
-
-i = 0 
-j = 1
-count = 0
-PL = "Profit/Losses"
-collumnCount = df[PL].count()
-change_sum = 0
-
-while j < collumnCount:
-    n = df.at[i,PL]
-    n1 = df.at[j,PL]
-    change = n1 - n 
-    change_sum += change
-    i = i+ 1
-    j = j+1
-
-averageChange = "{:.2f}".format(change_sum/(monthCount-1))
-
-print(f"Average Change: ${averageChange}")
-
-#Calculating and Printin Max Loss and Gain Along With Corresponding Month
-df["Monthly Change"] = df[PL].diff()
-
-maxGain = int(df["Monthly Change"].max())
-maxLoss = int(df["Monthly Change"].min())
-maxGainRow = df["Monthly Change"].idxmax()
-maxLossRow = df["Monthly Change"].idxmin()
-maxGainDate = df.loc[maxGainRow,"Date"]
-maxLossDate = df.loc[maxLossRow,"Date"]
-
-print(f"Greatest Increase in Profits: {maxGainDate} ${maxGain}")
-
-print(f"Greatest Decrease in Profits: {maxLossDate} ${maxLoss}")
-
-#Prepping Variables for Results Array
+# Prepare the financial analysis summary
 header = """Financial Analysis
 ----------------------------"""
-months = f"Total Months: {monthCount}"
-total = f"Total: ${Total}"
-averageProfit = f"Average Change: ${averageChange}"
-greatestIncrease = f"Greatest Increase in Profits: {maxGainDate} ${maxGain}"
-greatestDecrease = f"Greatest Decrease in Profits: {maxLossDate} ${maxLoss}"
+total_months_summary = f"Total Months: {total_months}"
+total_summary = f"Total: ${total_profit_losses}"
+average_change_summary = f"Average Change: ${average_change:.2f}"
+greatest_increase_summary = f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})"
+greatest_decrease_summary = f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})"
 
-#Exporting File
-results = [header,months, total, averageProfit, greatestIncrease, greatestDecrease]
+# Print the financial analysis summary to the console
+print(header)
+print(total_months_summary)
+print(total_summary)
+print(average_change_summary)
+print(greatest_increase_summary)
+print(greatest_decrease_summary)
 
-outputFilePath = file_path = "C:\\Users\\kidus\\OneDrive\\Documents\\GitHub\\python-challenge\\PyBank\\analysis\\PyBankOutput"
+# Specify the file path for exporting results
+output_file_path = "C:\\Users\\kidus\\OneDrive\\Documents\\GitHub\\python-challenge\\PyBank\\analysis\\PyBankOutput.txt"
 
-with open(outputFilePath, "w") as file:
-    for result in results:
-        file.write(result + "\n")
+# Export the financial analysis summary to a text file
+with open(output_file_path, "w") as file:
+    file.write(header + "\n")
+    file.write(total_months_summary + "\n")
+    file.write(total_summary + "\n")
+    file.write(average_change_summary + "\n")
+    file.write(greatest_increase_summary + "\n")
+    file.write(greatest_decrease_summary + "\n")
 
-print("Results exported to", outputFilePath)
-
-
-
-
-
-
+# Print a message indicating where the results were exported
+print("Financial analysis results exported to", output_file_path)
--- PyPoll/main.py/PyPoll.py
+++ PyPoll/main.py/PyPoll.py
@@ -1,89 +1,76 @@
-import pandas as pd
+import csv
 
-#Load in files 
+# Specify the file path
 file_path = "C:\\Users\\kidus\\OneDrive\\Documents\\GitHub\\python-challenge\\PyPoll\\Resources\\PyPoll.csv"
 
-df = pd.read_csv(file_path)
+# Initialize variables
+voteCount = 0
+TVcharlie, TVdiana, TVray = 0, 0, 0
 
-#Header
+# Open CSV file and count votes
+with open(file_path, 'r') as file:
+    reader = csv.reader(file)
+    header_row = next(reader)
+   
+    for row in reader:
+        voteCount += 1
+        if row[2] == "Charles Casper Stockham":
+            TVcharlie += 1
+        elif row[2] == "Diana DeGette":
+            TVdiana += 1
+        elif row[2] == "Raymon Anthony Doane":
+            TVray += 1
 
+# Header
 header = """Election Results
 ----------------------------"""
-
 print(header)
 
-#Find Total Vote Count
-voteCount = df["Ballot ID"].value_counts().sum()
-
+# Total Votes
 votes = f"Total Votes: {voteCount}"
-
 print(votes)
 
-#Spacer
-
+# Spacer
 spacer1 = "----------------------------"
 print(spacer1)
 
-#Count of Votes and Percentages for Charlie
-
-charlie = df["Candidate"] == "Charles Casper Stockham"
-
-TVcharlie = charlie.sum()
-PCTcharlie = "{:.3f}".format((TVcharlie/voteCount)*100)
-
-fincharlie =  f"Charles Casper Stockham: {PCTcharlie}% ({TVcharlie})"
-
+# Count of Votes and Percentages for Charlie
+PCTcharlie = "{:.3f}".format((TVcharlie / voteCount) * 100)
+fincharlie = f"Charles Casper Stockham: {PCTcharlie}% ({TVcharlie})"
 print(fincharlie)
 
-#Count of Votes and Percentages for Diana
-
-diana = df["Candidate"] == "Diana DeGette"
-
-TVdiana = diana.sum()
-PCTdiana = "{:.3f}".format((TVdiana/voteCount)*100)
-
-findiana =  f"Diana DeGette: {PCTdiana}% ({TVdiana})"
-
+# Count of Votes and Percentages for Diana
+PCTdiana = "{:.3f}".format((TVdiana / voteCount) * 100)
+findiana = f"Diana DeGette: {PCTdiana}% ({TVdiana})"
 print(findiana)
 
-#Count of Votes and Percentages for Ray
-
-ray = df["Candidate"] == "Raymon Anthony Doane"
-
-TVray = ray.sum()
-PCTray = "{:.3f}".format((TVray/voteCount)*100)
-
-finray =  f"Raymon Anthony Doane: {PCTray}% ({TVray})"
-
+# Count of Votes and Percentages for Ray
+PCTray = "{:.3f}".format((TVray / voteCount) * 100)
+finray = f"Raymon Anthony Doane: {PCTray}% ({TVray})"
 print(finray)
 
-#Spacer
-
+# Spacer
 spacer2 = "----------------------------"
 print(spacer2)
 
-#Winner Announcment 
-if TVcharlie > TVdiana:
-    if TVcharlie > TVray:
-        winner = "Winner: Charles Casper Stockham"
-    else:
-        winner = "Winner: Raymon Anthony Doane"
-else: 
-    if TVdiana > TVray:
-        winner = "Winner: Diana DeGette"
-    else:
-        winner = "Winner: Raymon Anthony Doane"
+# Winner Announcement
+if TVcharlie > TVdiana and TVcharlie > TVray:
+    winner = "Winner: Charles Casper Stockham"
+elif TVdiana > TVray:
+    winner = "Winner: Diana DeGette"
+else:
+    winner = "Winner: Raymon Anthony Doane"
 
 print(winner)
-#Spacer
 
+# Spacer
 spacer3 = "----------------------------"
 print(spacer3)
 
-#Exporting File
+# Exporting File
 results = [header, votes, spacer1, fincharlie, findiana, finray, spacer2, winner, spacer3]
 
-outputFilePath = file_path = "C:\\Users\\kidus\\OneDrive\\Documents\\GitHub\\python-challenge\\PyPoll\\analysis\\PyPollOutput"
+outputFilePath = "C:\\Users\\kidus\\OneDrive\\Documents\\GitHub\\python-challenge\\PyPoll\\analysis\\PyPollOutput"
 
 with open(outputFilePath, "w") as file:
     for result in results:
